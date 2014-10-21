98f5591caca567106cd5ee49022fb927d6055d92
Dennis Fricke <dennis.fricke@flavia-it.de>
2014-10-21 14:40:08 +0200

    Added REST interface for products

--- app/controllers/Product.scala
+++ app/controllers/Product.scala
@@ -1,12 +1,13 @@
 package controllers
 
-import play.api.mvc._
-import play.api.libs.json._
+import models.Product
 import play.api.libs.concurrent.Execution.Implicits._
-import scala.concurrent.Future
+import play.api.libs.json._
+import play.api.mvc._
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.json.collection.JSONCollection
-import models.Product
+
+import scala.concurrent.Future
 
 /**
  * Author: Dennis Fricke
@@ -15,6 +16,10 @@ import models.Product
 object Product extends Controller with MongoController {
 	def collection = db.collection[JSONCollection]("products")
 
+	/**
+	 * Create a new product
+	 * @return
+	 */
 	def post = Action.async(parse.json) { request =>
 		request.body.validate[Product].map {
 			case product => {
@@ -26,16 +31,73 @@ object Product extends Controller with MongoController {
 					}
 				}
 			}
-		}.recoverTotal{
-			e => Future { BadRequest(JsError.toFlatJson(e)) }
+		}.recoverTotal {
+			e => Future {
+				BadRequest(JsError.toFlatJson(e))
+			}
 		}
 	}
 
-	def find(id: String) = Action.async(parse.anyContent) { request =>
-		val cursor = collection.find(Json.obj("_id" -> Json.obj("$oid" ->id))).cursor[Product]
+	/**
+	 * List all products
+	 * @return
+	 */
+	def list() = Action.async { request =>
+		val cursor = collection.find(Json.obj()).cursor[Product]
 		val futureResults = cursor.collect[List]()
 		futureResults.map {
 			case t => Ok(Json.toJson(t))
+		}
+	}
+
+	/**
+	 * Get a product by id
+	 * @param id
+	 * @return
+	 */
+	def find(id: String) = Action.async(parse.anyContent) { request =>
+		val cursor = collection.find(Json.obj("_id" -> Json.obj("$oid" -> id))).cursor[Product]
+		val futureResults = cursor.collect[List]()
+		futureResults.map {
+			case t => Ok(Json.toJson(t))
+		}
+	}
+
+	/**
+	 * Update a product by id
+	 * @param id
+	 * @return
+	 */
+	def update(id: String) = Action.async(parse.json) { request =>
+		request.body.validate[Product].map {
+			case product => {
+				val futureResult = collection.update(Json.obj("_id" -> Json.obj("$oid" -> id)), product)
+				futureResult.map {
+					case t => t.inError match {
+						case true => InternalServerError("%s".format(t))
+						case false => Ok(Json.toJson(product))
+					}
+				}
+			}
+		}.recoverTotal {
+			e => Future {
+				BadRequest(JsError.toFlatJson(e))
+			}
+		}
+	}
+
+	/**
+	 * Delete a product by id
+	 * @param id
+	 * @return
+	 */
+	def delete(id: String) = Action.async(parse.anyContent) { request =>
+		val futureResult = collection.remove(Json.obj("_id" -> Json.obj("$oid" -> id)), firstMatchOnly = true)
+		futureResult.map {
+			case t => t.inError match {
+				case true => InternalServerError("%s".format(t))
+				case false => Ok("success")
+			}
 		}
 	}
 
